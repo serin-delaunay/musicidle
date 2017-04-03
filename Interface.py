8a710d86ac81e965572b5c5a4cc11ee5e11b0044
Serin Delaunay <serin.delaunay@gmail.com>
2017-04-03 13:25:53 +0100

    Interface class handles game loop

---
+++ Interface.py
@@ -0,0 +1,30 @@
+
+# coding: utf-8
+from Presentation import Presentation
+from bearlibterminal import terminal as blt
+from FPSLimiter import FPSLimiter
+from vec import vec
+from Mouse import set_mouse_only, get_mouse_input
+
+class Interface(object):
+    presentation : Presentation
+    fps_limiter : FPSLimiter = FPSLimiter()
+    def __init__(self, presentation : Presentation):
+        set_mouse_only()
+        self.presentation = presentation
+        self.presentation.size.x = blt.state(blt.TK_WIDTH)
+        self.presentation.size.y = blt.state(blt.TK_HEIGHT)
+    def draw(self):
+        self.presentation.update()
+        blt.clear()
+        self.presentation.root.draw(vec(0,0))
+        blt.refresh()
+    def loop(self):
+        while not self.presentation.stop:
+            self.fps_limiter.wait()
+            self.draw()
+            while blt.has_input():
+                signal = get_mouse_input()
+                self.presentation.handle(signal)
+                if self.presentation.stop:
+                    break
